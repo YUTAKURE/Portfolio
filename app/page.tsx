d89765f1d7335207d882dd2ba98d36dc633cc703
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-05-21 01:57:54 -0700

    ABOUTページ追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,123 @@
+'use client';
+
+import Image from 'next/image';
+import img1 from '../public/images/IMG_6190.jpeg';
+import { Button } from '@/components/ui/button';
+import { motion } from 'framer-motion';
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiBootstrap,
+  SiShadcnui,
+  SiSupabase,
+  SiGit,
+  SiGithub,
+} from 'react-icons/si';
+import { FaXTwitter, FaInstagram, FaGithub, FaLinkedin } from 'react-icons/fa6';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main>
+      <section>
+        <div className="flex container mx-auto py-10 flex-col md:flex-row items-center justify-center">
+          <motion.div
+            className="md:w-1/2 lg:pr-12 md:pr-8 text-center md:text-left"
+            initial={{ opacity: 0, translateX: -40, scale: 0.95 }}
+            animate={{ opacity: 1, translateX: 0, scale: 1 }}
+            transition={{ ease: 'easeInOut', duration: 0.5, delay: 0.1 }}
+          >
+            <h1 className="text-3xl sm:text-6xl font-medium mb-4 leading-snug sm:leading-snug">
+              Hello, <br className="hidden md:inline-block" />
+              <span className=" text-transparent bg-clip-text bg-gradient-to-r  from-blue-400 via-pink-500 to-yellow-300 ">
+                I&apos;m YUTA <br />
+              </span>
+              Web Developer
+            </h1>
+            <motion.p
+              className="mb-8 leading-relaxed"
+              initial={{ opacity: 0, translateX: -40, scale: 0.95 }}
+              animate={{ opacity: 1, translateX: 0, scale: 1 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              vel quam quas suscipit, obcaecati illum voluptas est hic
+              voluptatum dolore?
+            </motion.p>
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.5 }}
+            >
+              <Button
+                variant="outline"
+                className="mb-5 duration-300 bg-gradient-to-br from-blue-400 via-pink-500 to-yellow-300 "
+              >
+                Contact
+              </Button>
+            </motion.span>
+          </motion.div>
+          <motion.div
+            className="md:w-1/2 lg:max-w-lg w-3/4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1, delay: 0.6 }}
+          >
+            <Image
+              src={img1}
+              alt="YUTA"
+              width={590}
+              height={729}
+              objectFit="contain"
+              placeholder="blur"
+            />
+          </motion.div>
+        </div>
+      </section>
+
+      <section
+        id="about"
+        className="flex container mx-auto py-10 flex-col md:flex-row items-center justify-center"
+      >
+        <div>
+          <div>aaaaa</div>
+          <div className=" flex gap-3 md:gap-5 text-2xl md:text-5xl">
+            <FaXTwitter className=" hover:scale-110 duration-300" />
+            <FaInstagram className=" hover:scale-110 duration-300" />
+            <FaGithub className=" hover:scale-110 duration-300" />
+            <FaLinkedin className=" hover:scale-110 duration-300" />
+          </div>
+        </div>
+      </section>
+
+      <section
+        id="skill"
+        className="flex container mx-auto py-10 flex-col md:flex-row items-center justify-center h-screen"
+      >
+        <div>
+          <div>bbbbb</div>
+          <div className=" flex gap-3 md:gap-5 text-2xl md:text-5xl mb-3">
+            <SiHtml5 className=" hover:scale-110 duration-300" />
+            <SiCss3 className=" hover:scale-110 duration-300" />
+            <SiJavascript className=" hover:scale-110 duration-300" />
+            <SiTypescript className=" hover:scale-110 duration-300" />
+            <SiReact className=" hover:scale-110 duration-300" />
+            <SiNextdotjs className=" hover:scale-110 duration-300" />
+          </div>
+          <div className=" flex gap-3 md:gap-5 text-2xl md:text-5xl">
+            <SiTailwindcss className=" hover:scale-110 duration-300" />
+            <SiBootstrap className=" hover:scale-110 duration-300" />
+            <SiShadcnui className=" hover:scale-110 duration-300" />
+            <SiSupabase className=" hover:scale-110 duration-300" />
+            <SiGit className=" hover:scale-110 duration-300" />
+            <SiGithub className=" hover:scale-110 duration-300" />
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -27,15 +27,15 @@ const components: { title: string; href: string }[] = [
     href: '/docs/primitives/alert-dialog',
   },
   {
-    title: 'WORKS',
+    title: 'SKILL',
     href: '/docs/primitives/hover-card',
   },
   {
-    title: 'CONTACT',
+    title: 'WORKS',
     href: '/docs/primitives/progress',
   },
   {
-    title: 'Scroll-area',
+    title: 'CONTACT',
     href: '/docs/primitives/scroll-area',
   },
 ];
@@ -43,63 +43,82 @@ const components: { title: string; href: string }[] = [
 const Header = () => {
   const { setTheme } = useTheme();
   return (
-    <div className="px-8 py-10 flex items-center justify-between">
-      <Link
-        href="/"
-        className="font-medium text-lg md:text-2xl md:ml-4 lg:text-4xl"
-      >
+    <div className="px-8 py-5 mb-8 flex items-center justify-between border-b">
+      <Link href="/" className="font-medium text-lg md:text-2xl lg:text-3xl">
         YK Portfolio
       </Link>
 
-      <nav className="hidden md:flex gap-10 mr-5 justify-between items-center ">
-        <Link href="/about">ABOUT</Link>
-        <Link href="/about">WORKS</Link>
-        <Link href="/about">CONTACT</Link>
-        <Link href="/about">Scroll-area</Link>
-      </nav>
+      <div className="flex items-center gap-10">
+        <nav className="hidden md:flex gap-10">
+          <Link
+            href="#about"
+            className="text-sm lg:text-lg hover:opacity-75 duration-300"
+          >
+            ABOUT
+          </Link>
+          <Link
+            href="#skill"
+            className="text-sm lg:text-lg hover:opacity-75 duration-300"
+          >
+            SKILL
+          </Link>
+          <Link
+            href="/"
+            className="text-sm lg:text-lg hover:opacity-75 duration-300"
+          >
+            WORKS
+          </Link>
+          <Link
+            href="/"
+            className="text-sm lg:text-lg hover:opacity-75 duration-300"
+          >
+            CONTACT
+          </Link>
+        </nav>
 
-      <div className="flex items-center md:hidden md:gap-6">
-        <div>
-          <NavigationMenu>
-            <NavigationMenuList>
-              <NavigationMenuItem>
-                <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
-                <NavigationMenuContent>
-                  <ul className="grid w-[130px] gap-3 p-4">
-                    {components.map((component) => (
-                      <ListItem
-                        key={component.title}
-                        title={component.title}
-                        href={component.href}
-                      ></ListItem>
-                    ))}
-                  </ul>
-                </NavigationMenuContent>
-              </NavigationMenuItem>
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-        <div>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme('light')}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme('dark')}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme('system')}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+        <div className="flex sm:gap-2">
+          {/* sm:Menu */}
+          <div className="md:hidden">
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[130px] gap-3 p-4">
+                      {components.map((component) => (
+                        <ListItem
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                        ></ListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+
+          {/* Dark mode */}
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme('light')}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme('dark')}>
+                  Dark
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         </div>
       </div>
     </div>
