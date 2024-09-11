e05d4acb8ade09a660ba0c2b878b4ac5ec1cd9ba
YUTAKURE <shiro.white.ffffff@gmail.com>
2024-09-11 10:17:25 -0700

    resume修正

--- components/Resume.tsx
+++ components/Resume.tsx
@@ -1,9 +1,19 @@
 const Resume = () => {
   return (
     <div style={{ height: '100vh', width: '100%' }}>
-      <iframe
+      {/* <iframe
         src="/images/resume.pdf"
         style={{ width: '100%', height: 'calc(100% - 50px)', border: 'none' }}
+        title="Resume"
+      /> */}
+      <iframe
+        src="/images/resume.pdf"
+        style={{
+          width: '100%',
+          height: '100%',
+          border: 'none',
+          display: 'block', // スクロールバーが不要になるように
+        }}
         title="Resume"
       />
     </div>
